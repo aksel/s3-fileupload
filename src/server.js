0c0528532dd2a021c3edfd0ae502f5884be2bd50
torgard <aksel@akseltorgard.com>
2017-07-15 23:30:05 +0200

    Basic multipart fileupload with multer.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const multer = require('multer');
 
 const PORT = process.env.PORT || 5000;
+const MAX_FILE_SIZE = 10000000;
+const MIME_TYPES = /image\/(png|jpg|jpeg|bmp)/;
 
 const app = express();
 
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: MAX_FILE_SIZE },
+  fileFilter: (req, file, cb) => cb(null, MIME_TYPES.test(file.mimetype)),
+});
+
 app.get('/ping', (req, res) => res.send('hello world'));
+
+app.post('/', upload.single('image'), (req, res, next) => {
+  console.log(req.file);
+  res.sendStatus(200);
+});
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => res.sendStatus(err.status ? err.status : 500));
